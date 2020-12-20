e68471fef947335ec643d5995edb78ae4771d33c
mkavcic99 <mkavcic99@gmail.com>
2020-12-20 16:55:35 +0100

    sestevanje

---
+++ hello.py
@@ -0,0 +1,12 @@
+def hello(s):
+    print("Hello " + s)
+
+def sestevanje(a, b):
+    rezultat = 0
+    rezultat += a
+    rezultat += a
+    return rezultat
+
+
+hello("student")
+print(sestevanje(1, 3))
